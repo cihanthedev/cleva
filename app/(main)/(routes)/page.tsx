08a4629c6e104e49719fabdf082cb5127757b227
Cihan <cihnyilmz@gmail.com>
2024-03-18 22:32:58 +0100

    Update Authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,14 +1,9 @@
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-
-const state = true;
 
 export default function Home() {
   return (
-    <div>
-      <UserButton
-        afterSignOutUrl="/"
-      />
+    <div className="h-screen">
+      <UserButton />
     </div>
-  );
+  )
 }
